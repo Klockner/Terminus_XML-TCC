a1903d502dbaa6f0825f3464a4b3af179d3cd033
Gabriel Klockner <gabriel.klockner@gmail.com>
2016-05-16 22:43:44 -0300

    Implemented integration with view to test 3 completed

--- src/main/java/Program/Provas/Prova3.java
+++ src/main/java/Program/Provas/Prova3.java
@@ -27,7 +27,8 @@ public class Prova3 {
     }
     
     //Finds especific element in the nodes
-    public void init() {
+    public String init() {
+        StringBuilder sb = new StringBuilder();
         NodeList nodeList = doc.getElementsByTagName("produto-de-moda");
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node nNode = nodeList.item(i);
@@ -41,22 +42,54 @@ public class Prova3 {
                     System.out.println("Marca: " + element.getElementsByTagName("marca").item(0).getTextContent());
                     System.out.println("Loja: " + element.getElementsByTagName("loja").item(0).getTextContent());
                     System.out.println("Preco: " + element.getElementsByTagName("preco").item(0).getTextContent());
-                    System.out.println("Preco Promoção: " + element.getElementsByTagName("preco-promocao").item(0).getTextContent());
-                    System.out.println("Número de parcelas: " + element.getElementsByTagName("numero-de-parcelas").item(0).getTextContent());
+                    System.out.println("Preco promocao: " + element.getElementsByTagName("preco-promocao").item(0).getTextContent());
+                    System.out.println("Numero de parcelas: " + element.getElementsByTagName("numero-de-parcelas").item(0).getTextContent());
                     System.out.println("Valor das parcelas: " + element.getElementsByTagName("valor-da-parcela").item(0).getTextContent());
                     System.out.println("URL: " + element.getElementsByTagName("url").item(0).getTextContent());
                     System.out.println("URL da imagem: " + element.getElementsByTagName("url-imagem").item(0).getTextContent());
-                    System.out.println("Preenchimento: " + element.getElementsByTagName("preenchimento").item(0).getTextContent());
-                    System.out.println("Cor: " + element.getElementsByTagName("cor").item(0).getTextContent());
-                    System.out.println("Textura: " + element.getElementsByTagName("textura").item(0).getTextContent());
-                    System.out.println("Tamanho: " + element.getElementsByTagName("tamanho").item(0).getTextContent());
+                    System.out.println("Preenchimento: ");
+                    for (int countColor = 0; countColor < element.getElementsByTagName("cor").getLength(); countColor++) {
+                        System.out.println("    Cor: " + element.getElementsByTagName("cor").item(countColor).getTextContent());
+                    }
+                    for (int countTexture = 0; countTexture < element.getElementsByTagName("textura").getLength(); countTexture++) {
+                        System.out.println("    Textura: " + element.getElementsByTagName("textura").item(countTexture).getTextContent());
+                    }
+                    for (int j = 0; j < element.getElementsByTagName("tamanho").getLength(); j++) {
+                        System.out.println("Tamanho: " + element.getElementsByTagName("tamanho").item(j).getTextContent());
+                    }
                     System.out.println("PlusSize: " + element.getElementsByTagName("plus-size").item(0).getTextContent());
-                    System.out.println("Classificação: " + element.getElementsByTagName("classificacao").item(0).getTextContent());
+                    System.out.println("Classificacao: " + element.getElementsByTagName("classificacao").item(0).getTextContent());
                     System.out.println("Categoria: " + element.getElementsByTagName("categoria").item(0).getTextContent());
                     System.out.println("Subcategoria: " + element.getElementsByTagName("subcategoria").item(0).getTextContent());
+                    
+                    
+                    sb.append("SKU: ").append(element.getElementsByTagName("sku").item(0).getTextContent());
+                    sb.append("\nNome: ").append(element.getElementsByTagName("nome").item(0).getTextContent());
+                    sb.append("\nDescrição: ").append(element.getElementsByTagName("descricao").item(0).getTextContent());
+                    sb.append("\nMarca: ").append(element.getElementsByTagName("marca").item(0).getTextContent());
+                    sb.append("\nLoja: ").append(element.getElementsByTagName("loja").item(0).getTextContent());
+                    sb.append("\nPreco: ").append(element.getElementsByTagName("preco").item(0).getTextContent());
+                    sb.append("\nPreco promocao: ").append(element.getElementsByTagName("preco-promocao").item(0).getTextContent());
+                    sb.append("\nNumero de parcelas: ").append(element.getElementsByTagName("numero-de-parcelas").item(0).getTextContent());
+                    sb.append("\nURL: ").append(element.getElementsByTagName("url").item(0).getTextContent());
+                    sb.append("\nURL da imagem: ").append(element.getElementsByTagName("url-imagem").item(0).getTextContent());
+                    sb.append("\nPreenchimento: ");
+                     for (int countColor = 0; countColor < element.getElementsByTagName("cor").getLength(); countColor++) {
+                        sb.append("\n   Cor: ").append(element.getElementsByTagName("cor").item(countColor).getTextContent());
+                    }
+                    for (int countTexture = 0; countTexture < element.getElementsByTagName("textura").getLength(); countTexture++) {
+                        sb.append("\n   Textura: ").append(element.getElementsByTagName("textura").item(countTexture).getTextContent());
+                    }
+                    for (int j = 0; j < element.getElementsByTagName("tamanho").getLength(); j++) {
+                        sb.append("\nTamanho: ").append(element.getElementsByTagName("tamanho").item(j).getTextContent());
+                    }
+                    sb.append("\nPlusSize: ").append(element.getElementsByTagName("plus-size").item(0).getTextContent());
+                    sb.append("\nClassificacao: ").append(element.getElementsByTagName("classificacao").item(0).getTextContent());
+                    sb.append("\nCategoria: ").append(element.getElementsByTagName("categoria").item(0).getTextContent());
+                    sb.append("\nSubcategoria: ").append(element.getElementsByTagName("subcategoria").item(0).getTextContent());
                 }
             }
         }
+        return sb.toString();
     }
-    
 }
